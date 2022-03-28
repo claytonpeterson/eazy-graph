22c381355b9ed86c7f4c6ccaf5076a4120fc36c4
Clay Peterson <claytonpetersonmail@gmail.com>
2022-03-28 14:19:24 -0400

    added tests directory

---
+++ Tests/Editor/SerializerTests/XMLSerializerTests.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+public class XMLSerializerTests
+{
+    [Test]
+    public void TestSaving()
+    {
+        int numberOne = 1;
+        int numberTwo = 1;
+
+        Assert.That(numberOne == numberTwo, "numbers are not equal");
+    }
+
+    [Test]
+    public void TestLoading()
+    {
+
+    }
+}
