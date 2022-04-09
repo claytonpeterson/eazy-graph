325eece6795474ac16ab75f4f29d7b03b1af53bb
Clay Peterson <claytonpetersonmail@gmail.com>
2022-04-08 22:53:59 -0400

    created graph controller class

---
+++ Editor/Eazy Window/Window/View/Graph Controller/GraphController.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class GraphController
+{
+    private readonly NodeCreator nodeCreator;
+    private readonly NodeConnector nodeConnector;
+
+    public GraphController (NodeCreator nodeCreator, NodeConnector nodeConnector)
+    {
+        this.nodeCreator = nodeCreator;
+        this.nodeConnector = nodeConnector;
+    }
+}
