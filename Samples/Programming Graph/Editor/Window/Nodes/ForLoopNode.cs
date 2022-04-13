6249c282abefa1c7bfe09e6b7a542d0360e4de12
Clay Peterson <claytonpetersonmail@gmail.com>
2022-04-13 18:31:54 -0400

    moved IContainsValue into EazyGraph instead of the math sample

--- Samples/Programming Graph/Editor/Window/Nodes/ForLoopNode.cs
+++ Samples/Programming Graph/Editor/Window/Nodes/ForLoopNode.cs
@@ -1,11 +1,22 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 namespace skybirdgames.eazygraph.Samples.Programming.Editor
 {
-    /*public class ForLoopNode : NodeView, IContainsValue, IUpdate
+    public class ForLoopNode : NodeView, IContainsValue, IUpdate
     {
-        
-    }*/
+        public ForLoopNode(Vector2 position, TestingOutData data) : base(position, data)
+        {
+
+        }
+
+        public void Update()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public int Value()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
 }
