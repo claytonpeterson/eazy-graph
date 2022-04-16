60703cbd2af01e4d835d2c70c5025f3c41d2407c
Clay Peterson <claytonpetersonmail@gmail.com>
2022-04-15 23:05:11 -0400

    NodeView now has method to expose Port.GetPort

--- Editor/Window/View/Nodes/NodeView.cs
+++ Editor/Window/View/Nodes/NodeView.cs
@@ -38,19 +38,7 @@ namespace skybirdgames.eazygraph.Editor
         {
             return (TestingOutData)userData;
         }
-/*
-        public Port CreatePort(Direction portDirection, Port.Capacity capacity = Port.Capacity.Single, string portName = "")
-        {
-            var newPort = InstantiatePort(
-                Orientation.Horizontal,
-                portDirection,
-                capacity,
-                typeof(float));
 
-            newPort.portName = portName;
-            return newPort;
-        }
-*/
         public virtual void Refresh()
         {
             RefreshExpandedState();
@@ -59,25 +47,7 @@ namespace skybirdgames.eazygraph.Editor
 
         public Port GetPort(string portName)
         {
-            foreach (var child in inputContainer.Children())
-            {
-                var port = (Port)child;
-                if (port == null)
-                    continue;
-
-                if (port.portName == portName)
-                    return port;
-            }
-            foreach (var child in outputContainer.Children())
-            {
-                var port = (Port)child;
-                if (port == null)
-                    continue;
-
-                if (port.portName == portName)
-                    return port;
-            }
-            return null;
+            return Ports.GetPort(portName);
         }
 
         public abstract void Update();
